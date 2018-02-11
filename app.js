93137b5c19d43942e122e08f4e0243b59c8493c9
Rob Swiger <35196117+rob-swiger@users.noreply.github.com>
2018-02-11 10:02:20 -0500

    Added header, filtering by sport
    ### Added
    - Maroon header
    - Filtering by sport selector

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const vm = new Vue({
   el: '#app',
   data: {
-    results: []
+    results: [],
+    selected: ''
   },
   mounted() {
-    axios.get("https://newsapi.org/v2/everything?q=virginia%20tech%20hokies&pageSize=100&sortBy=publishedAt&apiKey=c0c101ad637a40188ac8ca2319bdb3c9")
+    axios.get("https://newsapi.org/v2/everything?q=virginia%20tech%20hokies&pageSize=50&sortBy=publishedAt&apiKey=c0c101ad637a40188ac8ca2319bdb3c9")
     .then(response => {this.results = response.data.articles})
-  }
+  },
+
+  methods: {
+         onChange: function (){
+         	var self = this
+          if (this.selected == 'All Hokie Sports') {
+            axios.get("https://newsapi.org/v2/everything?q=virginia%20tech%20hokies&pageSize=50&sortBy=publishedAt&apiKey=c0c101ad637a40188ac8ca2319bdb3c9")
+            .then(response => {this.results = response.data.articles})
+          } else {
+            axios.get('https://newsapi.org/v2/everything?q=virginia%20tech%20hokies%20'+this.selected+'&pageSize=50&sortBy=publishedAt&apiKey=c0c101ad637a40188ac8ca2319bdb3c9')
+             .then(response => {this.results = response.data.articles})
+          }
+         }
+    }
 });
